bbe29030a93280985dff164eabeb712b62363b74
nguyenkhoa <nguyenkhoa72.work@gmail.com>
2022-08-27 10:32:04 +0700

    consumer reader

--- api/index.js
+++ api/index.js
@@ -1,1 +1,47 @@
-console.log("helloword")
+const amqplib = require('amqplib');
+const amqpUrl = `amqp://host.docker.internal:5672`;
+
+
+
+const MQSubChannel = async (option) => {
+    const connection = await amqplib.connect(amqpUrl, 'heartbeat=60');
+    const channel = await connection.createChannel();
+    channel.prefetch(option.prefetch || 10);
+
+    process.once('SIGINT', async () => {
+        console.log('got sigint, closing connection');
+        await channel.close();
+        await connection.close();
+        process.exit(0);
+    });
+
+    await channel.assertQueue(option.queue, { durable: false });
+    return channel;
+};
+const main = async ()=>{
+    const SubChannel = await MQSubChannel({
+        prefetch:1,
+        queue: "events"
+    });
+    
+    process.once('SIGINT', async () => {
+        console.log('got sigint, closing connection');
+        await SubChannel.close();
+        await connection.close();
+        process.exit(0);
+    });
+    await SubChannel.consume(
+       'events', 
+        async (msg) => {
+            const decodeMsg = JSON.parse(msg.content.toString());
+            console.log(decodeMsg);
+            SubChannel.ack(msg);
+        },
+        {
+            noAck: false,
+            consumerTag: 'consumer',
+        }
+    );
+}
+
+main();
